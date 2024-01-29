5f06469d7d1dd419868879b0aeb1166b85ab0463
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2024-01-29 11:10:45 +0500

    Change request method `post`

--- src/main/java/com/example/simpleapi/controllers/user_controller.java
+++ src/main/java/com/example/simpleapi/controllers/user_controller.java
@@ -4,21 +4,18 @@ package com.example.simpleapi.controllers;
 import com.example.simpleapi.models.Auth;
 import com.example.simpleapi.token.Token;
 import com.example.simpleapi.token.TokenHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class user_controller {
 
-    @RequestMapping(value = "/user/login",method = RequestMethod.GET)
-    public String Login()
+    @RequestMapping(value = "/user/login",method = RequestMethod.POST)
+    public String Login(Auth auth)
     {
         var token = new Token();
         var tokenHandler = new TokenHandler();
         token.Id = 1;
-        token.Username = "Ehson";
+        token.Username = auth.Username;
         token.Age = 18;
 
         return tokenHandler.generateToken(token);
