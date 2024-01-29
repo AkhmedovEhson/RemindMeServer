9c7f4eeb03ce8205781f77673c1ed5c676a5d3be
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2024-01-29 19:53:13 +0500

    Provided jwt security middleware

--- src/main/java/com/example/simpleapi/security/JwtAuthorizationSecurity.java
+++ src/main/java/com/example/simpleapi/security/JwtAuthorizationSecurity.java
@@ -1,2 +1,20 @@
-package com.example.simpleapi.security;public class JwtAuthorizationSecurity {
+package com.example.simpleapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class JwtAuthorizationSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(O -> O.requestMatchers("/user/login").permitAll()
+                                .anyRequest().authenticated()
+                        );
+
+        return http.build();
+    }
 }
